3afca5f5508978496433958c9750deda72b1a68e
jkeithry <33007520+jkeithry@users.noreply.github.com>
2017-11-08 20:54:40 -0500

    Letter count
    Added code which counts each letter in every valid word. The count is updated in an array "letterFreq".

--- SnowmanPlayer.java
+++ SnowmanPlayer.java
@@ -12,7 +12,19 @@ public class SnowmanPlayer{
 	
 	public static void startGame(String[] words, int minLength, int maxLength, String allowedChars)
 	{
-		//count induvidual letter freqencies in the words array	
+		/*count induvidual letter freqencies in the words array*/		
+		int wordCount = 0;		
+		int[] letterFreq = new int[26];
+		//test each word
+		for( String w : words){			
+			wordCount++ ; 
+			//test each char (in each word) that appears in allowedChars
+			for(int i = 0; i < w.length(); i++){
+				char c = w.charAt(i);
+				int letter = allowedChars.indexOf(c);
+				letterFreq[letter]++;				
+			}
+		}	
 	}
 	
 	public static void startNewWord(int length)

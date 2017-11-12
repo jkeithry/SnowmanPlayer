01261b33cc13c45d3e40f81ce38710374e020bd9
jkeithry <33007520+jkeithry@users.noreply.github.com>
2017-11-11 19:17:18 -0500

    Update SnowmanPlayer.java

--- SnowmanPlayer.java
+++ SnowmanPlayer.java
@@ -1,7 +1,20 @@
 import java.util.stream.*;
+import java.util.Arrays;
 public class SnowmanPlayer{		
-private static int[] letterFreq5 ;
-private static char[] guessSequence = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	private static  int[] letterFreq5 = new int[26];
+	private static  int[] letterFreq6 = new int[26];
+	private static  int[] letterFreq7 = new int[26];
+	private static  int[] letterFreq8 = new int[26];
+	private static  int[] letterFreq9 = new int[26];
+	private static  int[] letterFreq10 = new int[26];
+	private static  int[] letterFreq11 = new int[26];
+	private static  int[] letterFreq12 = new int[26];
+	private static  int[] letterFreq13 = new int[26];
+	private static  int[] letterFreq14 = new int[26];
+	private static  int[] letterFreq15 = new int[26];
+	private static String guessSequence = "abcdefghijklmnopqrstuvwxyz";
+	
+
 	public static void main(String args[])
 	{	
 	}
@@ -17,113 +30,17 @@ private static char[] guessSequence = {'a','b','c','d','e','f','g','h','i','j','
 	public static void startGame(String[] words, int minLength, int maxLength, String allowedChars)
 	{
 		/*count induvidual letter freqencies in the words array*/	
-		int[] letterFreq5 = new int[26];
-		int[] letterFreq6 = new int[26];
-		int[] letterFreq7 = new int[26];
-		int[] letterFreq8 = new int[26];
-		int[] letterFreq9 = new int[26];
-		int[] letterFreq10 = new int[26];
-		int[] letterFreq11 = new int[26];
-		int[] letterFreq12 = new int[26];
-		int[] letterFreq13 = new int[26];
-		int[] letterFreq14 = new int[26];
-			
-		
-		//test each word
-		for( String w : words){						
-			/*Test each char (in each word) that appears in allowedChars.
-			Make a different freqencie profile depending on word length.*/
-			if(w.length() == 5){
-				for(int i = 0; i < w.length(); i++){
-					char c = w.charAt(i);
-					int letter = allowedChars.indexOf(c);
-					letterFreq5[letter]++;				
-				}
-			}
-			if(w.length() == 6){
-				for(int i = 0; i < w.length(); i++){
-					char c = w.charAt(i);
-					int letter = allowedChars.indexOf(c);
-					letterFreq6[letter]++;				
-				}
-			}
-			if(w.length() == 7){
-				for(int i = 0; i < w.length(); i++){
-					char c = w.charAt(i);
-					int letter = allowedChars.indexOf(c);
-					letterFreq7[letter]++;				
-				}
-			}
-			if(w.length() == 8){
-				for(int i = 0; i < w.length(); i++){
-					char c = w.charAt(i);
-					int letter = allowedChars.indexOf(c);
-					letterFreq8[letter]++;				
-				}
-			}
-			if(w.length() == 9){
-				for(int i = 0; i < w.length(); i++){
-					char c = w.charAt(i);
-					int letter = allowedChars.indexOf(c);
-					letterFreq9[letter]++;				
-				}
-			}
-			if(w.length() == 10){
-				for(int i = 0; i < w.length(); i++){
-					char c = w.charAt(i);
-					int letter = allowedChars.indexOf(c);
-					letterFreq10[letter]++;				
-				}
-			}
-			if(w.length() == 11){
-				for(int i = 0; i < w.length(); i++){
-					char c = w.charAt(i);
-					int letter = allowedChars.indexOf(c);
-					letterFreq11[letter]++;				
-				}
-			}
-			if(w.length() == 12){
-				for(int i = 0; i < w.length(); i++){
-					char c = w.charAt(i);
-					int letter = allowedChars.indexOf(c);
-					letterFreq12[letter]++;				
-				}
-			}
-			if(w.length() == 13){
-				for(int i = 0; i < w.length(); i++){
-					char c = w.charAt(i);
-					int letter = allowedChars.indexOf(c);
-					letterFreq13[letter]++;				
-				}
-			}
-			if(w.length() == 14){
-				//first letter of the word
-				char firstChar = w.charAt(0);
-				int letter = allowedChars.indexOf(firstChar);
-				letterFreq14[letter]++;
-				// for(int i = 0; i < w.length(); i++){
-					// char c = w.charAt(i);
-					// int letter = allowedChars.indexOf(c);
-					// letterFreq14[letter]++;				
-				// }
-			}			
-		}
-		//print array and stuff to the console
-		for(int i = 0; i < allowedChars.length(); i++){
-			System.out.print(allowedChars.charAt(i) + " ");
-		}
-		System.out.print("\n");
-			
-		for(int i : letterFreq14){
-			System.out.print(i + " ");
-		}
-		System.out.print("\n");
-		
-		int sum = IntStream.of(letterFreq14).sum();
-		System.out.println("The sum is " + sum);
-		
-		System.out.print("\n");		
-		//end of print stuff
+		letterFreq5 = wordFreqMaker(5, words);
+		letterFreq6 = wordFreqMaker(6, words);
+		letterFreq7 = wordFreqMaker(7, words);
+		letterFreq8 = wordFreqMaker(8, words);
+		letterFreq9 = wordFreqMaker(9, words);
+		letterFreq10 = wordFreqMaker(10, words);
+		letterFreq11 = wordFreqMaker(11, words);
+		letterFreq12 = wordFreqMaker(12, words);
+		letterFreq13 = wordFreqMaker(13, words);
+		letterFreq14 = wordFreqMaker(14, words);
+		letterFreq15 = wordFreqMaker(15, words);		
 	}	
 	/**	
 	*Analyses new word and applies letterFreq profile based on word length.
@@ -131,40 +48,105 @@ private static char[] guessSequence = {'a','b','c','d','e','f','g','h','i','j','
 	*/
 	public static void startNewWord(int length)
 	{		
-		if(length == 5){
-			//guessSequence = letterFreq5;
+		if(length == 5){		
+			guessSequence = arrayToString(letterFreq5);		
 		}
 		if(length == 6){
+			guessSequence = arrayToString(letterFreq6);
 		}
 		if(length == 7){
+			guessSequence = arrayToString(letterFreq7);
 		}
-		if(length == 8){
+		if(length == 8){ 
+			guessSequence = arrayToString(letterFreq8);
 		}
 		if(length == 9){
+			guessSequence = arrayToString(letterFreq9);
 		}
 		if(length == 10){
+			guessSequence = arrayToString(letterFreq10);
 		}
 		if(length == 11){
+			guessSequence = arrayToString(letterFreq11);
 		}
 		if(length == 12){
+			guessSequence = arrayToString(letterFreq12);
 		}
 		if(length == 13){
+			guessSequence = arrayToString(letterFreq13);
 		}
 		if(length == 14){
-		}			
+			guessSequence = arrayToString(letterFreq14);
+		}
+		if(length == 15){			
+			guessSequence = arrayToString(letterFreq15);
+		}
+			
 	}	
 	/**	
 	*@param args
 	*/
 	public static char guessLetter(String pattern, String previousGuesses)
 	{
+		//make guess based on first letter, second letter etc.
+		
 		//guess from the letter frequency sets			
 		int count = previousGuesses.length();
-		char result = guessSequence[count];
+		char result = guessSequence.charAt(count);
 		count++;		
 		return result;		
 	}
-	private static int[] 
-	
-	
+	/**
+	*converts array data to a usable string for guessLetter
+	*/
+	private static String arrayToString(int[] frequency){		
+		//didnt sort properly without arraycopy method???????????????????
+		int [] tmp = new int[frequency.length];
+		System.arraycopy( frequency, 0, tmp, 0, frequency.length );
+		Arrays.sort(tmp);		
+		String alpha = "abcdefghijklmnopqrstuvwxyz";
+		String reversed = "";
+		String result ;
+		for(int i = 0; i < tmp.length; i++){			
+			for(int j = 0; j < frequency.length; j++){
+				if(tmp[i] == frequency[j]){
+					reversed += alpha.charAt(j);
+				}
+			}
+		}
+		result = reverse(reversed);
+		
+		return result;
+	}
+	/**
+	*Reverses the string given by arrayToString in order to get the proper order. I can probably just guess backwards?
+	*/
+	private static String reverse(String str){
+		String reversedStr;
+		if(str.length() == 1){	    
+			return str;
+		}
+		char first = str.charAt(0);
+		String remain = str.substring(1);
+		reversedStr = reverse(remain) + first;	
+		return reversedStr;	
+	}
+	/**
+	*Makes the letter occurance array based on word length.
+	*/
+	private static int[] wordFreqMaker(int length, String[] words){
+		String allowedChars = "abcdefghijklmnopqrstuvwxyz";
+		int[] result = new int[26];
+		for( String w : words){	
+			if(w.length() == length){
+				for(int i = 0; i < length; i++){
+					char c = w.charAt(i);
+					int letter = allowedChars.indexOf(c);
+					result[letter]++;			
+				}
+			}				
+		}	
+		return result;						
+	}	
 }
+

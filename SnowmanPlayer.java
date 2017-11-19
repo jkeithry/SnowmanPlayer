d5876b49876588cbd5178e9e1093ee140780de1b
jkeithry <33007520+jkeithry@users.noreply.github.com>
2017-11-19 09:47:02 -0500

    Update SnowmanPlayer.java

--- SnowmanPlayer.java
+++ SnowmanPlayer.java
@@ -1,16 +1,9 @@
-//191 misses ofer 20 words
-import java.util.stream.*;
-import java.util.Arrays;
 import java.util.*;
-import java.util.regex.Pattern;
+import java.util.Arrays;
 public class SnowmanPlayer{	
-	//Data extracted from dictionary. index 5 - 15 stores the letter occurances by word length
-	private static String[] letterFreq = new String[20];	
-	//words based on length
-	private static String[][] wordsByLen = new String[50][5000];	
-	// private static List<String> fiveLetterWords = new ArrayList<String>();
-	private static String guessSequence = "";
-
+	private static List<String> myWords = new ArrayList<String>();
+	
+	private static String chars = "";	
 	public static void main(String args[])
 	{	
 	}
@@ -22,87 +15,145 @@ public class SnowmanPlayer{
 		return "Keith, Jeffrey";
 	}	
 	/**	
+	*	
 	*/
 	public static void startGame(String[] words, int minLength, int maxLength, String allowedChars)
-	{
-		/*count induvidual letter freqencies in the words array*/			
-		for(int i = minLength; i <= maxLength;i++){
-			letterFreq[i] = wordFreqMaker(i, words);
-		}		
-		for(int i = minLength; i <= maxLength;i++){
-			List<String> wordsLen = new ArrayList<String>();
-			for( String w : words){
-				if( w.length() == i){
-					wordsLen.add(w);
-				}				
-			}
-			wordsByLen[i] = wordsLen.toArray(wordsByLen[i]);			
-		}
-		//printing wordsByLen		
-		// for(int i = minLength; i < maxLength; i++){
-			// String str = wordFreqMaker(i, wordsByLen[i]);
-			// System.out.println(str);	
-		// }
-	
-	}		
-	/**	
-	*Analyses new word and applies letterFreq profile based on word length.
-	*@param takes in and int word length
-	*/
-	public static void startNewWord(int length)
 	{				
-		guessSequence = letterFreq[length];				
-	}	
+		for( String w : words){	//Gathers all the words into a new arraylist.
+			myWords.add(w);							
+		}		
+	}
+	//current working words
+	private static List<String> myWorkingWords = new ArrayList<String>();//List of words currently working with.
+	private static int totalGuess = 0;// counts total guesses.
+	private static int crctCnt = 0;// counts correct guesses.
+	private static int prevCrctCnt = 0;
 	/**	
-	*this is going to get messy
+	*Analyses new word. Set workingChars to a string with the highest occuring letter first.
 	*@param args
 	*/
+	public static void startNewWord(int length)	
+	{		
+		myWorkingWords = wordFilter(myWords, length);	//send myWords to wordFilter to return myWorkingWords.
+		chars = "abcdefghijklmnopqrstuvwxyz"; //testing string
+		//chars = occuranceMaker(myWorkingWords, length);//sends myWorkingWords to freqMaker.
+		totalGuess = 0;// guesses are zero for a new word.
+		crctCnt = 0;
+		prevCrctCnt = 0;
+		
+	}	
+	private static String lastGuess = "";
+	/**	
+	*Narrow myWorkingWords to words that match the pattern and do not contain previousGuesses(misses)
+	*@param args 
+	*/
 	public static char guessLetter(String pattern, String previousGuesses)
-	{			
-		//First guess from the letter frequency array	
-		String nothing = new String(new char[pattern.length()]).replace("\0", "*");
-		int count = previousGuesses.length();		
-		int cnt = 0;
-		char result = guessSequence.charAt(count);
-		//System.out.println("GS0 : " + guessSequence);
-		outer:		
-		while(true){				
-			if(pattern.equals(nothing)){		
-				result = guessSequence.charAt(count);
-				// System.out.println("GS1 : " + guessSequence);
-			}
-			else{
-				String[] arr = wordFilter(wordsByLen[pattern.length()], pattern);			
-				guessSequence = wordFreqMaker(pattern.length(), arr);
-				result = guessSequence.charAt(cnt);
-				System.out.println("GS2 : " + guessSequence);
-			}
-			if(previousGuesses.indexOf(result) != -1){cnt++; continue outer;}
-			else{break;}
+	{	
+		// System.out.println(pattern);
+		char result ;
+		//was the last guess correct?	
+		boolean crctGuess = false;
+		crctCnt = correctCounter(pattern);//count correct guesses.		
+		if(crctCnt > prevCrctCnt){ crctGuess = true; prevCrctCnt = crctCnt;}
+				
+		if(totalGuess == 0){
+			result = chars.charAt(totalGuess);//use the most frequent letter if no correct guess has been made.
+		}	
+		else if (crctGuess){// a new guess has been made. Refine myWorkingWords based on the guess.		
+			myWorkingWords = wordFilter(myWorkingWords, lastGuess, pattern, crctGuess);
+			chars = occuranceMaker(myWorkingWords, pattern.length());
+			result = chars.charAt(0);
+			// result = chars.charAt(totalGuess);
 		}
-		System.out.println(pattern);
-		return result;		
+		else{//Filter words based on the correct guesses.
+			myWorkingWords = wordFilter(myWorkingWords, lastGuess, pattern, crctGuess);
+			chars = occuranceMaker(myWorkingWords, pattern.length());	
+			result = chars.charAt(0);
+		}
+		// System.out.println(Arrays.toString(myWorkingWords.toArray()));		
+		//Ensure the guess has not already been made;
+		boolean badG = true;
+		int altCnt = 0;
+		while(badG){			
+			if(previousGuesses.indexOf(result) > -1){			
+			result = chars.charAt(altCnt++);
+			}
+			else{badG = false;}
+		}			
+		lastGuess = Character.toString(result);		
+		totalGuess++;		
+		return result;
+		
 	}
+	/**	
+	*This word filter is used for filtering words out that don't match the known pattern
+	*@param args
+	*/
+	private static List<String> wordFilter(List<String> words, String lastGuess, String pattern, boolean crctGuess){			
+		List<String> filteredWords = new ArrayList<String>(words);
+		List<String> addFilteredWords = new ArrayList<String>();
+		if(crctGuess){
+			for(int i = 0; i < words.size(); i++){
+				if(words.get(i).indexOf(lastGuess) == pattern.indexOf(lastGuess)){
+					addFilteredWords.add(words.get(i));
+				}
+			}return addFilteredWords;
+		}
+		else{
+			for(int i = 0; i < words.size(); i++){				
+				if(words.get(i).contains(lastGuess)) {filteredWords.remove(words.get(i));}		
+			}			
+		}
+		return filteredWords;	
+	}
+	/**	
+	*@param args
+	*/
+	private static List<String> wordFilter(List<String> words, int length){			
+		List<String> filteredWords = new ArrayList<String>();			
+		for(int i = 0; i < words.size(); i++){
+			if(words.get(i).length() == length){filteredWords.add(words.get(i));}
+		}			
+		return filteredWords;
+	}
+	/**
+	*Makes the letter occurance array based on word length.
+	*/
+	private static String occuranceMaker(List<String> words, int length){		
+		String allowedChars = "abcdefghijklmnopqrstuvwxyz";
+		int[] ltrFrq = new int[26];
+		for( String w : words){	
+			//if(w.length() == length){
+			for(int i = 0; i < length; i++){
+				char c = w.charAt(i);
+				int letter = allowedChars.indexOf(c);
+				ltrFrq[letter]++;			
+			}
+						
+		}
+		//returns the array count of letters as a string in order of most common letter first.	
+		//System.out.println(Arrays.toString(ltrFrq));
+		return occuranceToString(ltrFrq);						
+	}	
 	/**
 	*converts array data of 26 elements into a string from most frequent to least frequent occurance.
 	*/
-	private static String arrayToString(int[] frequency){		
-		
-		int [] tmp = new int[frequency.length];
-		System.arraycopy( frequency, 0, tmp, 0, frequency.length );
+	private static String occuranceToString(int[] occurance){		
+		int [] tmp = new int[occurance.length];
+		System.arraycopy( occurance, 0, tmp, 0, occurance.length );
 		Arrays.sort(tmp);		
 		String alpha = "abcdefghijklmnopqrstuvwxyz";
 		String reversed = "";
 		String result ;
+		//this can probably be written in one loop.
 		for(int i = 0; i < tmp.length; i++){			
-			for(int j = 0; j < frequency.length; j++){
-				if(tmp[i] == frequency[j]){
+			for(int j = 0; j < occurance.length; j++){
+				if(tmp[i] == occurance[j]){
 					reversed += alpha.charAt(j);
 				}
 			}
-		}
-		result = reverse(reversed);
-		
+		}		
+		result = reverse(reversed);		
 		return result;
 	}
 	/**
@@ -118,44 +169,13 @@ public class SnowmanPlayer{
 		reversedStr = reverse(remain) + first;	
 		return reversedStr;	
 	}
-	/**
-	*Makes the letter occurance array based on word length.
-	*/
-	private static String wordFreqMaker(int length, String[] words){
-		String allowedChars = "abcdefghijklmnopqrstuvwxyz";
-		int[] arrayOfFreq = new int[26];
-		for( String w : words){	
-			if(w.length() == length){
-				for(int i = 0; i < length; i++){
-					char c = w.charAt(i);
-					int letter = allowedChars.indexOf(c);
-					arrayOfFreq[letter]++;			
-				}
-			}				
-		}
-		//returns the array count of letters as a string in order of most common letter first.
-		return arrayToString(arrayOfFreq);						
-	}	
-	private static String[] wordFilter(String[] words, int length){			
-		List<String> filteredWords = new ArrayList<String>();			
-		//String[] result = new String[26];
-		for(int i = 0; i < words.length; i++){
-			if(words[i].length() == length){filteredWords.add(words[i]);}
-		}		
-		String[] result = filteredWords.toArray(new String[26]);
-		return result;
-	}
-			
-	private static String[] wordFilter(String[] words, String pattern){
-		List<String> filteredWords = new ArrayList<String>();		
-		for(int i = 0; i < words.length; i++){
-			for(int j = 0; j < pattern.length(); j++){
-				if(words[i].charAt(j) == pattern.charAt(j)){
-					filteredWords.add(words[i]);
-				}						
-			}			
-		}	
-		String[] result = filteredWords.toArray(new String[26]);
-		return result;		
+	private static int correctCounter(String pattern){
+		String correct = "";
+		for(int i = 0 ; i < pattern.length(); i++){
+				if(pattern.charAt(i) != '*'){correct = correct + pattern.charAt(i);}
+			}
+		return correct.length();
 	}	
 }
+
+
